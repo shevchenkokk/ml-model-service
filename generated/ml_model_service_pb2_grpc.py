037b861b668dfbc15a455c89d7e9756d50b0ef69
shevchenkokk <kirill_07_02@mail.ru>
2025-11-05 11:53:06 +0300

    corrected import in generated code

--- generated/ml_model_service_pb2_grpc.py
+++ generated/ml_model_service_pb2_grpc.py
@@ -4,7 +4,7 @@ import grpc
 import warnings
 
 from google.protobuf import empty_pb2 as google_dot_protobuf_dot_empty__pb2
-from proto import ml_model_service_pb2 as proto_dot_ml__model__service__pb2
+from generated import ml_model_service_pb2 as proto_dot_ml__model__service__pb2
 
 GRPC_GENERATED_VERSION = '1.76.0'
 GRPC_VERSION = grpc.__version__
